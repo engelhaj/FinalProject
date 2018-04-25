6fad8d958118d6888ed62d5526a9fd6315cb0944
Vincent Abuelo <abuelov@student.psdr3.org>
2018-04-25 12:12:24 -0500

    Help Button

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,13 +1,17 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements PanelChangeListener{
 
 	private JPanel contentPane;
+	private PanelChangeListener listener;
 
 	/**
 	 * Launch the application.
@@ -33,8 +37,35 @@ public class MainFrame extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnHelp = new JButton("Help");
+		btnHelp.setBounds(6, 50, 117, 29);
+		btnHelp.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				listener.changePanel("MainFrame");
+			}
+			
+		});
+		contentPane.add(btnHelp);
 	}
 
+	@Override
+	public void changePanel(String panelName) {
+		// TODO Auto-generated method stub
+		JPanel newPanel = null;
+		
+		if(panelName.equals("RulesPanel")){
+			newPanel = new RulesPanel();
+		}
+		
+		if(newPanel != null){
+			setContentPane(newPanel);
+			validate();
+		}
+	}
 }
