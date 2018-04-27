d7649d7619c5ee6af20f536c2c7ac46e57d5a50a
Joshua Engelhardt <engelhaj@student.psdr3.org>
2018-04-27 11:59:19 -0500

    Player Added

---
+++ src/Player.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Player {
+	private double balance;
+	private ArrayList<Card> hand;
+	
+	public Player(){
+		hand = new ArrayList<>();
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	public double changeBalance(double amount){
+		balance = balance - (amount*-1);
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public ArrayList<Card> getHand() {
+		return hand;
+	}
+	public void setHand(ArrayList<Card> hand) {
+		this.hand = hand;
+	}
+	public void addCard(Card card){
+		hand.add(card);
+	}
+}
